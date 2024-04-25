2138c65fe5392224594aed03b5f5da4eb03b2e40
Lava Ahmed <lavaahmed915@gmail.com>
2024-04-25 20:38:48 +0300

    completed assignment :)

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const saturdayFun = (activity = "roller-skate") => `This Saturday, I want to ${activity}!`;
+
+const mondayWork = (activity = "go to the office") => `This Monday, I will ${activity}.`;
+
+const wrapAdjective = (par) => {
+  return function (x = "special") {
+    return `You are ${par}${x}${par}!`;
+  };
+};
+
+const encouragingPromptFunction = wrapAdjective("*");
